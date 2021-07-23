9c53de6ed2399957bdd033f25f6d026580328ea5
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-23 11:37:31 -0400

    Thing class filled out (simplest type of creature), repurposed Room class

--- libs/room.js
+++ libs/room.js
@@ -1,10 +1,8 @@
-const Creature = require('./creature');
+const Thing = require('./creature');
 const Vector = require('./vector');
 
 class Room {
     constructor(name, floor, rangeSlider, originX, originY, size, pop){
-        //range slider in controls
-        this.rangeSlider = rangeSlider;
 
         this.originX = originX;
         this.originY = originY;
@@ -13,38 +11,29 @@ class Room {
         this.size = size;
 
         this.pop = pop; //population
-        this.creatures = new Array(pop);
-        // for (var c = 0; c < pop; c++)
-        //     this.creatures[c] = new Creature(this, new Vector(rand(0, size), rand(0, size))); //random positions
-        this.creatures[0] = new Creature(this, new Vector(rand(0, size), rand(0, size)), 0x0000ff); //random positions
-        this.creatures[1] = new Creature(this, new Vector(rand(0, size), rand(0, size))); //random positions
+        this.things = new Array(pop);
+        for (var c = 0; c < pop; c++)
+            this.things[c] = new Thing(this, new Vector(rand(0, size), rand(0, size))); //random positions
     }
 
     update() {
         for (var c=0; c < this.pop; c++) 
-            this.creatures[c].update();
+            this.things[c].update();
 
         this.draw();
-        //TODO: Fix genetic algorithm
-        if (frameCount % generationLength == 0){
-            // this.genetics();
-            this.creatures[0].mutate(1);
+        if (worldCount % generationLength == 0){
+            this.creatureGenetics();
         }
     }
 
     draw() {
-        // graphics.lineStyle(2, 0x800000); 
-        // //graphics.beginFill(0xff0000);
-        // graphics.drawRoundedRect(this.originX, this.originY, this.size, this.size, 10);
-        // graphics.endFill();
+        //(this.originX, this.originY, this.size, this.size, 10);
     }
 
-    //TODO: Debug genetic algorithm
-    genetics() {
-        //fitness sorting, more fit creatures move to front
-        this.creatures.sort((b, a) => (a.fitness > b.fitness) ? 1 : -1);
-
-        this.pop = this.creatures.length;
+    creatureGenetics() {
+        //fitness sorting function in which more fit things move to front
+        this.things.sort((b, a) => (a.fitness > b.fitness) ? 1 : -1);
+        this.pop = this.things.length;
     }
 
 }
--- libs/thing.js
+++ libs/thing.js
@@ -1,7 +1,69 @@
-const neat = require('neataptic');
+const Net = require('./net');
 
 class Thing {
-    constructor(network) {
-        this.network = network; //or create copy here
+    constructor(room, vector, color = 0xffffff){
+        this.room = room;
+        this.color = color;
+        this.position = vector.getCopy();
+        this.velocity = new Vector(0, 0, -3, 3);
+        this.accel = new Vector(0, 0, -0.1, 0.1);
+
+        //Feedforward Neural Network
+        this.net = new Net(2, 5, 2);
+    }
+
+    draw() {
+        //(this.position.x + this.room.originX, this.position.y + this.room.originY, 1);
+    }
+
+    update() {
+        //console.log(this.network);
+
+        //get this room's size and its red dot
+        var roomSize = this.room.size;
+        
+        //normalize positions by room size (for NN inputs)
+        var input = [1,1];
+        var output = this.net.activate(input);
+
+        //the vector to be changed by output, such as velocity or accel
+        var changing = this.velocity; 
+
+        //update creature's acceleration from output
+        changing.x = output[0] - 0.5;
+        changing.y = output[1] - 0.5;
+        
+        outOfBoundsCheck();
+
+        //add acceleration to velocity
+        this.velocity.add(this.accel);
+        //add velocity to position
+        this.position.add(this.velocity);
+
+        this.draw();
+    }
+
+    replace() {}
+
+    outOfBoundsCheck() {
+        var rebound = 5;
+        if (this.position.x > this.room.size-rebound){
+            this.velocity.x = -this.velocity.x;
+            this.position.x -= rebound;
+        }
+        if (this.position.y > this.room.size-rebound){
+            this.velocity.y = -this.velocity.y;
+            this.position.y -= rebound;
+        }
+        if (this.position.x < rebound){
+            this.velocity.x  = -this.velocity.x;
+            this.position.x += rebound;
+        }
+        if (this.position.y < rebound){
+            this.velocity.y = -this.velocity.y;
+            this.position.y += rebound;
+    }
     }
 }
+
+module.exports = Thing;
