a3e1f9544a012aaf9594b425a9795b411e8b5622
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-23 20:48:52 -0400

    working on making net more dynamic

--- libs/net.js
+++ libs/net.js
@@ -2,12 +2,10 @@
 //Just a perceptron/feedforward net for now
 class Net {
 	constructor(...layerSizes)
-	{
-		this.numLayers = layerSizes.length;
-		
+	{		
 		//initialize all charges to zero
-		this.charges = new Array(this.numLayers);
-		for (let i = 0; i < this.numLayers; i++)
+		this.charges = new Array(layerSizes.length);
+		for (let i = 0; i < layerSizes.length; i++)
 		{
 			this.charges[i] = new Array(this.layerSizes[i].length);
 			for (let j = 0; j < this.layerSizes[i].length; j++)
@@ -16,22 +14,21 @@ class Net {
 		
 		
 		//fully interconnected random weights 
-		this.weights = new Array(this.numLayers);
-		for (let i = 0; i < this.numLayers; i++)
+		this.weights = new Array(layerSizes.length);
+		for (let i = 0; i < layerSizes.length - 1; i++)
 		{
 			this.weights[i] = new Array(this.layerSizes[i].length);
-			if (i < this.numLayers - 1) //this prevents the net from linking the output to a non-existent layer
-				for (let j = 0; j < this.layerSizes[i].length; j++)
-				{
-					this.weights[i][j] = new Array(this.layerSizes[i + 1])
-					for (let k = 0; k < this.layerSizes[i + 1].length; k++)
-						this.weights[i][j][k] = Math.random();
-				}
+			for (let j = 0; j < this.layerSizes[i].length; j++)
+			{
+				this.weights[i][j] = new Array(this.layerSizes[i + 1])
+				for (let k = 0; k < this.layerSizes[i + 1].length; k++)
+					this.weights[i][j][k] = Math.random();
+			}
 		}
 		
 		//random thresholds
-		this.thresholds = new Array(this.numLayers);
-		for (let i = 0; i < this.numLayers - 1; i++)
+		this.thresholds = new Array(layerSizes.length);
+		for (let i = 0; i < layerSizes.length - 1; i++)
 		{
 			this.weights[i] = new Array(this.layerSizes[i].length);
 			for (let j = 0; j < this.layerSizes[i].length; j++)
@@ -40,6 +37,9 @@ class Net {
 				else
 					thresholds[i][j] = Math.random() * 10;
 		}
+
+		//save layerSizes
+		this.layerSizes = layerSizes;
 	}
 
 	activate(input) {
@@ -48,40 +48,53 @@ class Net {
 		this.charges[0] = input;
 
 		//charges propogate forward
-		for (let i = 0; i <this.numLayers; i++)
-		{
-			if (i <this.numLayers - 1) //this prevents the net from feeding outputs to a non-existent layer
-				for (let j = 0; j <this.layerSizes[i].length; j++)
-					if (i == 0 ||this.charges[i][j] > this.thresholds[i][j])
-						for (let k = 0; k <this.layerSizes[i + 1].length; k++)
+		//each layer
+		for (let i = 0; i < this.charges.length - 1; i+) {
+			//each neuron
+			for (let j = 0; j < this.charges[i].length; j++) {
+				//charge checked against threshold
+				if (i == 0 ||this.charges[i][j] > this.thresholds[i][j]) {
+					//each weight
+					for (let k = 0; k <this.layerSizes[i + 1].length; k++)
 						this.charges[i + 1][k] +=this.charges[i][j] * this.weights[i][j][k];
+				}
+			}
+			//squish next layer after its been all charged up
+			for (let k = 0; k <this.layerSizes[i + 1].length; k++) 
+
+
 		}
 		//return output layer
 		return this.charges[this.numLayers-1];;
 	}
 
 	//copy over this net's weights with another net's weights, mutating in the process
-	replaceAndMutate (otherNet, mutationRate=0)
-	{ 
-	//weights overwritten
-	for (var i = 0; i < otherNet.numLayers; i++)
-	{
-		for (var j = 0; j < otherNet.layerSizes[i].length; j++)
+	replaceAndMutate (otherNet, mutationRate=0) { 
+		//weights overwritten
+		for (var i = 0; i < otherNet.numLayers; i++)
 		{
-			for (var k = 0; k < otherNet.layerSizes[i].length; k++)
-			{	
-				this.weights[i][j] = otherNet.weights[i][j];
-				if (Math.random() < mutRate)
+			for (var j = 0; j < otherNet.layerSizes[i].length; j++)
+			{
+				for (var k = 0; k < otherNet.layerSizes[i].length; k++)
+				{	
+					this.weights[i][j] = otherNet.weights[i][j];
+					if (Math.random() < mutRate)
+				}
 			}
 		}
+		//thresholds overwritten
+		for (var i = 0; i < otherNet.numLayers - 1; i++)
+		{
+			for (var j = 0; j < otherNet.layerSizes[i].length; j++)
+				this.thresholds[i][j] = otherNet.thresholds[i][j];
+		}
 	}
-	//thresholds overwritten
-	for (var i = 0; i < otherNet.numLayers - 1; i++)
-	{
-		for (var j = 0; j < otherNet.layerSizes[i].length; j++)
-			this.thresholds[i][j] = otherNet.thresholds[i][j];
-	}
-}
+
+	//activation functions
+	//for output layer
+	sigmoid(x) { return 1 / (1 + Math.exp(-x)); };
+	//for hidden layer
+	relu(x) { return Math.max(0, x); };
 }
 
 module.exports = Net;
