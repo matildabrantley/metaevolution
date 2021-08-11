fe7a522a839c8a000d2f368c05ad8ee94963cfc8
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-10 21:04:09 -0400

    Species is named Group again since Species is now a new class that contains a collection of Groups that have the ability to share genes on occasion. Groups are basically isolated local populations i.e. subspecies. I chose Group over Subspecies or Population since it extends Phaser's Group class. Each Group in a Species will have the same neural architecture allowing for direct sexual reproduction.

---
+++ public/js/group.js
@@ -0,0 +1,178 @@
+// const Life = require('./life');
+// const Vector = require('./vector');
+
+class Group extends Phaser.Physics.Arcade.Group {
+    constructor(world, scene, config, goals, isKey = false){
+        super(world, scene, config);
+        this.lives = [];
+        this.scene = scene;
+        this.world = world;
+
+        this.timer1 = 0;
+        this.goals = goals.getChildren();
+        this.bonusLength = 1;
+        this.bonusGoal = 0;
+        this.isKey = isKey;
+
+        //for updateFast()
+        this.timer2 = 0;
+        this.fastGenLength = 50;
+    }
+
+    //maintains array of Life objects, sets initial distances from goals and create Minds
+    setup(mutRate = 0.05, selectionCutoff = 0.1, maxGenLength = 500, initialGenLength = 10, deltaGenLength = 5) {
+        this.lives = this.getChildren();
+
+        this.mutRate = mutRate;
+        this.maxGenLength = maxGenLength;
+        this.genLength = initialGenLength;
+        this.deltaGenLength = deltaGenLength;
+        this.selectionCutoff = selectionCutoff;
+
+
+        for (let life of this.lives){
+            life.startingDistFromGoal = new Array(this.goals.length);
+            for (let g=0; g < this.goals.length; g++)
+                life.startingDistFromGoal[g] = Phaser.Math.Distance.BetweenPoints(life, this.goals[g]);
+        
+            //Create minds based on number of goals
+            life.mind = new Mind(this.goals.length * 3, 3);
+        }
+    }
+
+    //normal updating within Phaser's/Matter's loop
+    updateWithEngine() {
+        this.timer1++;
+        for (let life of this.lives) {
+            //if (this.timer1 % 10 == 0){
+                //for (let goal of this.goals)
+                    //goal.setVelocity((Math.random()-0.5) * 700, (Math.random()-0.5) * 700);
+            //}
+
+            life.update(this.goals, this.bonusGoal);
+
+            //Fitness to be managed by group
+            let distScores = [];
+            for (let g=0; g < this.goals.length; g++){
+                let newScore = life.startingDistFromGoal[g] / (Phaser.Math.Distance.BetweenPoints(life, this.goals[g]) + 1);
+                if (g == this.bonusGoal){
+                    newScore += 10; 
+                    newScore *= 2.5; 
+                    //this.goals[g].setScale(6);
+                }
+                else {
+                    newScore = -1;
+                    //this.goals[g].setScale(4);
+                }
+                    
+                distScores.push(newScore);
+            }
+            //use reducer to get total product (and divide by 1000 to keep values manageable)
+            life.fitness += distScores.reduce((a,b) => a+b, 1) / 1000;
+        }
+
+        if (this.timer1 % this.genLength == 0){
+           this.selection();
+        }
+        if (this.timer1 % this.bonusLength == 0){
+           this.bonusGoal = Math.floor((Math.random() * this.goals.length));
+        }
+    }
+
+    //simplified fast updating without a framework/renderer/engine, and very limited physics
+    async updateFast() {
+        this.timer2++;
+        for (let i=0; i < this.lives.length; i++) 
+            this.lives[i].updateFast();
+
+        if (this.timer2 % this.fastGenLength == 0){
+            this.selection();
+        }
+    }
+
+    //generational change in group where fitness is sorted and replacement and mutation occur
+    selection() {
+        if (this.genLength < this.maxGenLength)
+            this.genLength += this.deltaGenLength;
+        this.timer1 = 0;
+
+        this.bonusLength = Math.floor(this.genLength / 10);
+
+        //fitness sorting function in which more fit lives move to front
+        this.lives.sort((b, a) => (a.fitness > b.fitness) ? 1 : -1);
+
+        //vast majority of population replaced by sexual offspring of top X% (X = selectionCutoff)
+        for (let i=this.lives.length-20; i > this.lives.length * this.selectionCutoff; i--) {
+            let mom = Math.floor(Math.random() * Math.floor(this.lives.length * this.selectionCutoff));
+            let dad = Math.floor(Math.random() * Math.floor(this.lives.length * this.selectionCutoff));
+            this.lives[i].mind.net.sexual(this.lives[mom].mind.net, this.lives[dad].mind.net, this.mutRate);
+        }
+
+        //Elite Selection: Best 10 always get spot(s) in next generation without mutation
+        if (this.lives.length > 3) { //two clones of 1st and one clone of 2nd
+            this.lives[this.lives.length - 1].mind.net.asexual(this.lives[0].mind.net, 0);
+            this.lives[this.lives.length - 2].mind.net.asexual(this.lives[0].mind.net, 0);
+            this.lives[this.lives.length - 3].mind.net.asexual(this.lives[1].mind.net, 0);
+        } if (this.lives.length > 6) { //three children of mating 1st/2nd
+            this.lives[this.lives.length - 4].mind.net.sexual(this.lives[0].mind.net, this.lives[1].mind.net, 0);
+            this.lives[this.lives.length - 5].mind.net.sexual(this.lives[0].mind.net, this.lives[1].mind.net, 0);
+            this.lives[this.lives.length - 6].mind.net.sexual(this.lives[0].mind.net, this.lives[1].mind.net, 0);
+        } if (this.lives.length > 10) {   //mating between 1st/3rd and 2nd/3rd, and clones of 3rd and 4th      
+            this.lives[this.lives.length - 7].mind.net.sexual(this.lives[0].mind.net, this.lives[2].mind.net, 0);
+            this.lives[this.lives.length - 7].mind.net.sexual(this.lives[1].mind.net, this.lives[2].mind.net, 0);
+            this.lives[this.lives.length - 9].mind.net.asexual(this.lives[2].mind.net, 0);
+            this.lives[this.lives.length - 10].mind.net.asexual(this.lives[3].mind.net, 0);
+        } if (this.lives.length > 15) {         
+            this.lives[this.lives.length - 11].mind.net.asexual(this.lives[3].mind.net, 0);
+            this.lives[this.lives.length - 12].mind.net.asexual(this.lives[3].mind.net, 0);
+            this.lives[this.lives.length - 13].mind.net.asexual(this.lives[4].mind.net, 0);
+            this.lives[this.lives.length - 14].mind.net.asexual(this.lives[4].mind.net, 0);
+            this.lives[this.lives.length - 15].mind.net.asexual(this.lives[5].mind.net, 0);
+        } if (this.lives.length > 20) {         
+            this.lives[this.lives.length - 16].mind.net.asexual(this.lives[5].mind.net, 0);
+            this.lives[this.lives.length - 17].mind.net.asexual(this.lives[6].mind.net, 0);
+            this.lives[this.lives.length - 18].mind.net.asexual(this.lives[7].mind.net, 0);
+            this.lives[this.lives.length - 19].mind.net.asexual(this.lives[8].mind.net, 0);
+            this.lives[this.lives.length - 20].mind.net.asexual(this.lives[9].mind.net, 0);
+        }
+
+        //key group controls goals
+        // if (this.isKey){
+        //     for (let goal of this.goals)
+        //         goal.setPosition(200 + Math.random() * 400, 150 + Math.random() * 300);
+        // }
+
+        //reset
+        let newStartingX = 400;
+        let newStartingY = 300;
+        for (let life of this.lives){
+            life.setPosition(newStartingX, newStartingY);
+
+            for (let g=0; g < this.goals.length; g++)
+                life.startingDistFromGoal[g] = Phaser.Math.Distance.BetweenPoints(life, this.goals[g]);
+
+            life.fitness = 0;
+            
+        }
+    }
+
+    //Genetic flow from between population
+    //Best to call right after selection since they're already sorted
+    geneFlow(otherPopulation, flowRatio = 0.1) {
+        for (let i=0; i < this.lives.length * flowRatio; i++) {
+            //avoid replacing best % or the Elites that replaced the bottom 20
+            let replaced = randIntBetween(this.lives.length * this.selectionCutoff, this.lives.length - 20)
+            let mom = randIntBetween(0, this.lives.length);
+            let dad = randIntBetween(0, otherPopulation.length);
+            //mating between two populations
+            this.lives[replaced].mind.net.sexual(this.lives[mom].mind.net, otherPopulation.lives[dad].mind.net, 0);
+        }
+    }
+
+}
+
+const randIntBetween = (lowNum, highNum) => {
+    lowNum = Math.ceil(lowNum);
+    highNum = Math.floor(highNum);
+    return Math.floor(Math.random() * (highNum - lowNum + 1)) + lowNum;
+}
--- public/js/launch.js
+++ public/js/launch.js
@@ -3,182 +3,176 @@ let width = 800;
 let height = 600;
 let speciesPop = 150;
 let maxStars = 10;
+let group;
+let species
 
-    let group;
+var config = {
+    type: Phaser.AUTO,
+    width: width,
+    height: height,
+    backgroundColor: '#000F00',
+    parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+    },
+    scene: {
+        preload: preload,
+        create: create, 
+        update: update,
+        //render: render
+    }
+};
 
-    var config = {
-        type: Phaser.AUTO,
-        width: width,
-        height: height,
-        backgroundColor: '#000F00',
-        parent: 'phaser-example',
-        physics: {
-            default: 'arcade',
-        },
-        scene: {
-            preload: preload,
-            create: create, 
-            update: update,
-            //render: render
-        }
+var game = new Phaser.Game(config);
+let firstSpecies;
+let starGroup;
+let distTexts = [];
+let loneStar;
+let distances = [];
+
+function preload () {
+    this.load.image('star', 'sprites/star.png');
+    this.load.image('bluestar', 'sprites/bluestar.png');
+    this.load.image('blackstar', 'sprites/blackstar.png');
+    this.load.image('greenstar', 'sprites/greenstar.png');
+    this.load.image('circle', 'sprites/circle.png');
+    this.load.atlas('redLife', 'sprites/pulsing-red-dot.png', 'sprites/pulsing-red-dot.json');
+    this.load.atlas('blueLife', 'sprites/pulsing-blue-dot.png', 'sprites/pulsing-blue-dot.json');
+    this.load.atlas('greenLife', 'sprites/pulsing-green-dot.png', 'sprites/pulsing-green-dot.json');
+    this.load.atlas('brightLife', 'sprites/pulsing-white-star.png', 'sprites/pulsing-white-star.json');
+}
+
+function create () {
+    this.physics.world.setBounds( 0, 0, width, height );
+
+    let goalGroup = this.add.group();
+    loneStar = this.physics.add.image(width * 0.25, height * 0.25, 'star');
+    loneStar.setCircle(30);
+    loneStar.setScale(4);
+    loneStar.setBounce(5);
+    loneStar.collideWorldBounds = true;
+    goalGroup.add(loneStar);
+    
+    blueStar = this.physics.add.image(width * 0.75, height * 0.25, 'bluestar');
+    blueStar.setCircle(30);
+    blueStar.setScale(4);
+    blueStar.setBounce(5);
+    blueStar.collideWorldBounds = true;
+    goalGroup.add(blueStar);
+    
+    blackStar = this.physics.add.image(width * 0.75, height * 0.75, 'blackstar');
+    blackStar.setCircle(30);
+    blackStar.setScale(5);
+    blackStar.setBounce(5);
+    blackStar.collideWorldBounds = true;
+    goalGroup.add(blackStar);
+    
+    greenStar = this.physics.add.image(width * 0.25, height * 0.75, 'greenstar');
+    greenStar.setCircle(30);
+    greenStar.setScale(5);
+    greenStar.setBounce(5);
+    greenStar.collideWorldBounds = true;
+    goalGroup.add(greenStar);
+
+    //firstSpecies is key species, meaning is controls goals
+    firstSpecies = new Species(this.physics.world, this, config, goalGroup, true);
+    secondSpecies = new Species(this.physics.world, this, config, goalGroup);
+    thirdSpecies = new Species(this.physics.world, this, config, goalGroup);
+    fourthSpecies = new Species(this.physics.world, this, config, goalGroup);
+
+    const redAnimConfig = {
+        key: 'redKey',
+        frames: 'redLife',
+        frameRate: 30,
+        repeat: -1
     };
-    
-    var game = new Phaser.Game(config);
-    let firstSpecies;
-    let starGroup;
-    let distTexts = [];
-    let loneStar;
-    let distances = [];
-    
-    function preload () {
-        this.load.image('star', 'sprites/star.png');
-        this.load.image('bluestar', 'sprites/bluestar.png');
-        this.load.image('blackstar', 'sprites/blackstar.png');
-        this.load.image('greenstar', 'sprites/greenstar.png');
-        this.load.image('circle', 'sprites/circle.png');
-        this.load.atlas('redLife', 'sprites/pulsing-red-dot.png', 'sprites/pulsing-red-dot.json');
-        this.load.atlas('blueLife', 'sprites/pulsing-blue-dot.png', 'sprites/pulsing-blue-dot.json');
-        this.load.atlas('greenLife', 'sprites/pulsing-green-dot.png', 'sprites/pulsing-green-dot.json');
-        this.load.atlas('brightLife', 'sprites/pulsing-white-star.png', 'sprites/pulsing-white-star.json');
+    const blueAnimConfig = {
+        key: 'blueKey',
+        frames: 'blueLife',
+        frameRate: 30,
+        repeat: -1
+    };
+    const greenAnimConfig = {
+        key: 'greenKey',
+        frames: 'greenLife',
+        frameRate: 30,
+        repeat: -1
+    };
+    const brightAnimConfig = {
+        key: 'brightKey',
+        frames: 'brightLife',
+        frameRate: 60,
+        repeat: -1
+    };
+    this.anims.create(redAnimConfig);
+    this.anims.create(blueAnimConfig);
+    this.anims.create(greenAnimConfig);
+    this.anims.create(brightAnimConfig);
+
+
+    for (let i=0; i < speciesPop; i++){
+        let life = new Life(this, 300, 400, 'redLife', 'pulsing-red-dot0.png');
+        //life.setCircle(16);
+        life.play('redKey');
+        firstSpecies.add(life);
     }
-    
-    function create () {
-        this.physics.world.setBounds( 0, 0, width, height );
-
-        let goalGroup = this.add.group();
-        loneStar = this.physics.add.image(width * 0.25, height * 0.25, 'star');
-        loneStar.setCircle(30);
-        loneStar.setScale(4);
-        loneStar.setBounce(5);
-        loneStar.collideWorldBounds = true;
-        goalGroup.add(loneStar);
-        
-        blueStar = this.physics.add.image(width * 0.75, height * 0.25, 'bluestar');
-        blueStar.setCircle(30);
-        blueStar.setScale(4);
-        blueStar.setBounce(5);
-        blueStar.collideWorldBounds = true;
-        goalGroup.add(blueStar);
-        
-        blackStar = this.physics.add.image(width * 0.75, height * 0.75, 'blackstar');
-        blackStar.setCircle(30);
-        blackStar.setScale(5);
-        blackStar.setBounce(5);
-        blackStar.collideWorldBounds = true;
-        goalGroup.add(blackStar);
-        
-        greenStar = this.physics.add.image(width * 0.25, height * 0.75, 'greenstar');
-        greenStar.setCircle(30);
-        greenStar.setScale(5);
-        greenStar.setBounce(5);
-        greenStar.collideWorldBounds = true;
-        goalGroup.add(greenStar);
-
-        //firstSpecies is key species, meaning is controls goals
-        firstSpecies = new Species(this.physics.world, this, config, goalGroup, true);
-        secondSpecies = new Species(this.physics.world, this, config, goalGroup);
-        thirdSpecies = new Species(this.physics.world, this, config, goalGroup);
-        fourthSpecies = new Species(this.physics.world, this, config, goalGroup);
-
-        const redAnimConfig = {
-            key: 'redKey',
-            frames: 'redLife',
-            frameRate: 30,
-            repeat: -1
-        };
-        const blueAnimConfig = {
-            key: 'blueKey',
-            frames: 'blueLife',
-            frameRate: 30,
-            repeat: -1
-        };
-        const greenAnimConfig = {
-            key: 'greenKey',
-            frames: 'greenLife',
-            frameRate: 30,
-            repeat: -1
-        };
-        const brightAnimConfig = {
-            key: 'brightKey',
-            frames: 'brightLife',
-            frameRate: 60,
-            repeat: -1
-        };
-        this.anims.create(redAnimConfig);
-        this.anims.create(blueAnimConfig);
-        this.anims.create(greenAnimConfig);
-        this.anims.create(brightAnimConfig);
-
-
-        for (let i=0; i < speciesPop; i++){
-            let life = new Life(this, 300, 400, 'redLife', 'pulsing-red-dot0.png');
-            //life.setCircle(16);
-            life.play('redKey');
-            firstSpecies.add(life);
-        }
-        firstSpecies.setup(0.05);
-        for (let i=0; i < speciesPop; i++){
-            let life = new Life(this, 500, 300, 'blueLife', 'pulsing-blue-dot0.png');
-            //life.setCircle(16);
-            life.play('blueKey');
-            secondSpecies.add(life);
-        }
-        secondSpecies.setup(0.1, 0.2);
-        for (let i=0; i < speciesPop; i++){
-            let life = new Life(this, 500, 300, 'greenLife', 'pulsing-green-dot0.png');
-            //life.setCircle(16);
-            life.play('greenKey');
-            thirdSpecies.add(life);
-        }
-        thirdSpecies.setup(0.15);
-        for (let i=0; i < speciesPop; i++){
-            let life = new Life(this, 500, 300, 'brightLife', 'pulsing-white-star0.png');
-            //life.setCircle(16);
-            life.play('brightKey');
-            fourthSpecies.add(life);
-        }
-        fourthSpecies.setup(0.15);
-        //starGroup = this.add.group();
-        // for (let i=0; i < maxStars; i++){
-        //     let starBody = this.physics.add.image((i+1)*10 + 100, (i+1)*10, 'star');
-        //     starBody.setCircle(30);
-        //     starGroup.add(starBody);
-        // }
-
-        //for (let i=0; i < firstSpecies.getLength(); i++)
-         //   distTexts.push(this.add.text(500, i*50 + 20, 'hello'));
+    firstSpecies.setup(0.05);
+    for (let i=0; i < speciesPop; i++){
+        let life = new Life(this, 500, 300, 'blueLife', 'pulsing-blue-dot0.png');
+        //life.setCircle(16);
+        life.play('blueKey');
+        secondSpecies.add(life);
     }
-
-    function update () {
-        let i = 0;
-        // firstSpecies.children.each(function(creature) {
-        //     distTexts[i].setText(Phaser.Math.Distance.BetweenPoints(loneStar, creature));
-        //     i++;
-        // }, this);
-
-        // if (starGroup.getLength() < maxStars) {
-        //     let starBody = this.physics.add.image(Math.random() * width, Math.random() * height, 'star');
-        //     starBody.setCircle(30);
-        //     starGroup.add(starBody);
-        // }
-        firstSpecies.updateWithEngine();
-        secondSpecies.updateWithEngine();
-        thirdSpecies.updateWithEngine();
-        fourthSpecies.updateWithEngine();
-        //this.physics.collide(firstSpecies, loneStar, eat);
+    secondSpecies.setup(0.1, 0.2);
+    for (let i=0; i < speciesPop; i++){
+        let life = new Life(this, 500, 300, 'greenLife', 'pulsing-green-dot0.png');
+        //life.setCircle(16);
+        life.play('greenKey');
+        thirdSpecies.add(life);
     }
-
-    // function render () {
-    //     this.game.debug.text( "This is debug text", 100, 380 );
+    thirdSpecies.setup(0.15);
+    for (let i=0; i < speciesPop; i++){
+        let life = new Life(this, 500, 300, 'brightLife', 'pulsing-white-star0.png');
+        //life.setCircle(16);
+        life.play('brightKey');
+        fourthSpecies.add(life);
+    }
+    fourthSpecies.setup(0.15);
+    //starGroup = this.add.group();
+    // for (let i=0; i < maxStars; i++){
+    //     let starBody = this.physics.add.image((i+1)*10 + 100, (i+1)*10, 'star');
+    //     starBody.setCircle(30);
+    //     starGroup.add(starBody);
     // }
 
-    function eat (creature, food) {
-        food.destroy();
-        // creature.fitness++;
-    }
+    //for (let i=0; i < firstSpecies.getLength(); i++)
+     //   distTexts.push(this.add.text(500, i*50 + 20, 'hello'));
+}
 
-    // ​function getAllMethods(object) {
-    //     return Object.getOwnPropertyNames(object).filter(function(property) {
-    //         return typeof object[property] == 'function';
-    //     });
+function update () {
+    let i = 0;
+    // firstSpecies.children.each(function(creature) {
+    //     distTexts[i].setText(Phaser.Math.Distance.BetweenPoints(loneStar, creature));
+    //     i++;
+    // }, this);
+
+    // if (starGroup.getLength() < maxStars) {
+    //     let starBody = this.physics.add.image(Math.random() * width, Math.random() * height, 'star');
+    //     starBody.setCircle(30);
+    //     starGroup.add(starBody);
     // }
+    firstSpecies.updateWithEngine();
+    secondSpecies.updateWithEngine();
+    thirdSpecies.updateWithEngine();
+    fourthSpecies.updateWithEngine();
+    //this.physics.collide(firstSpecies, loneStar, eat);
+}
+
+// function render () {
+//     this.game.debug.text( "This is debug text", 100, 380 );
+// }
+
+function eat (creature, food) {
+    food.destroy();
+    // creature.fitness++;
+}
