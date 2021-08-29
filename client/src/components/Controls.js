06923c5c5f4baedd708fe25cd66fbbc7b38376bd
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-29 14:22:34 -0400

    New Controls component uses offcanvas sidebar.

--- client/src/components/Controls.js
+++ client/src/components/Controls.js
@@ -5,27 +5,25 @@ import { Button, Offcanvas } from 'react-bootstrap';
 
 const Controls = () => {
     const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
+    
     const handleShow = () => setShow(true);
-  
+    const handleHide = () => setShow(false);
+    
     return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch
-        </Button>
-  
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </Offcanvas.Body>
-        </Offcanvas>
-      </>
-    );
+        <div>
+            <Button variant="dark" onClick={handleShow}>
+                Controls
+            </Button>
+            <Offcanvas show={show} onHide={handleHide}>
+                <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Hey there, I'm over here on the side!
+                </Offcanvas.Body>
+            </Offcanvas>
+        </div>
+    )
 };
 
 export default Controls;
