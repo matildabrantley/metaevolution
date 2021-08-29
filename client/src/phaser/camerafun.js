7cb353123093ac32fb1de85b7f63d9b31676d612
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-28 21:08:00 -0400

    Converted schema of multiple background images with slightly different styles rotating into Phaser Image logic, using alpha instead of opacity and scene update instead of setInterval. This can be used in Ecosystem. Also can be used to indicate the performance of a species, e.g. red style when red species increasing average fitness faster.

--- client/src/phaser/camerafun.js
+++ client/src/phaser/camerafun.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import floral from './assets/sprites/floral-stunning.jpg';
+import floral2 from './assets/sprites/floral-colorful.jpg';
 
 class Camerafun extends Phaser.Scene
 {
@@ -11,31 +12,51 @@ class Camerafun extends Phaser.Scene
     preload () 
     {
         this.load.image('background', floral);
+        this.load.image('background2', floral2);
     }
 
     create () 
     {
-        this.cameras.main.setBounds(0, 0, 1440, 1440);
+        const cam = this.cameras.main.setBounds(0, 0, 1440, 1440);
 
 
 
-        const backgrounds = [];
-        backgrounds.push(this.add.image(0, 0, 'background').setOrigin(0));
-        backgrounds.push(this.add.image(720, 0, 'background').setOrigin(0));
-        backgrounds[1].flipX=true;
-        backgrounds.push(this.add.image(0, 720, 'background').setOrigin(0));
-        backgrounds[2].flipY=true;
-        backgrounds.push(this.add.image(720, 720, 'background').setOrigin(0));
-        backgrounds[3].flipX=true;
-        backgrounds[3].flipY=true;
+        this.bgs = [];
+        this.bgs.push(this.add.image(0, 0, 'background').setOrigin(0));
+        this.bgs.push(this.add.image(720, 0, 'background').setOrigin(0));
+        this.bgs[1].flipX=true;
+        this.bgs.push(this.add.image(0, 720, 'background').setOrigin(0));
+        this.bgs[2].flipY=true;
+        this.bgs.push(this.add.image(720, 720, 'background').setOrigin(0));
+        this.bgs[3].flipX=true;
+        this.bgs[3].flipY=true;
 
-        // backgrounds[1].flipX=true;
-
-        for (const background of backgrounds) {
+        for (const background of this.bgs) {
             background.setDisplaySize(2048, 2048);
             background.setScale(0.5);
-            background.alpha = 1;
+            background.alpha = 0.4;
         }
+
+        this.bgs2 = [];
+        this.bgs2.push(this.add.image(0, 0, 'background2').setOrigin(0));
+        this.bgs2.push(this.add.image(720, 0, 'background2').setOrigin(0));
+        this.bgs2[1].flipX=true;
+        this.bgs2.push(this.add.image(0, 720, 'background2').setOrigin(0));
+        this.bgs2[2].flipY=true;
+        this.bgs2.push(this.add.image(720, 720, 'background2').setOrigin(0));
+        this.bgs2[3].flipX=true;
+        this.bgs2[3].flipY=true;
+
+        for (const background of this.bgs2) {
+            background.setDisplaySize(2048, 2048);
+            background.setScale(0.5);
+            background.alpha = 0.2;
+        }
+        
+        this.maxOpacity = 0.5;
+        this.bgOpacity = this.maxOpacity;
+        this.bgOpacity2 = 0;
+        this.bgOpacityDirection = 1;
 
 
     
@@ -44,37 +65,50 @@ class Camerafun extends Phaser.Scene
     
         // this.cameras.main.originX = 1;
         this.cameras.main.centerToSize();
+        cam.setBackgroundColor(0xbababa);
     }
 
     update () 
     {
+        if (this.bgOpacity >= this.maxOpacity)
+            this.bgOpacityDirection = -1;
+        else if (this.bgOpacity <= 0)
+            this.bgOpacityDirection = 1;
+  
+        this.bgOpacity += 0.001 * this.bgOpacityDirection;
+        this.bgOpacity2 += 0.001 * this.bgOpacityDirection * -1;
+        
+        this.bgs.forEach(bg => { bg.alpha = this.bgOpacity })
+        this.bgs2.forEach(bg => { bg.alpha = this.bgOpacity2 })
+        
+
         const cam = this.cameras.main;
     
         if (this.keys.A.isDown)
         {
-            cam.scrollX -= 6;
+            cam.scrollX -= 4;
         }
         else if (this.keys.D.isDown)
         {
-            cam.scrollX += 6;
+            cam.scrollX += 4;
         }
     
         if (this.keys.W.isDown)
         {
-            cam.scrollY -= 6;
+            cam.scrollY -= 4;
         }
         else if (this.keys.S.isDown)
         {
-            cam.scrollY += 6;
+            cam.scrollY += 4;
         }
     
         if (this.cursors.left.isDown)
         {
-            cam.rotation -= 0.01;
+            cam.rotation -= 0.005;
         }
         else if (this.cursors.right.isDown)
         {
-            cam.rotation += 0.01;
+            cam.rotation += 0.005;
         }
     }
 }
