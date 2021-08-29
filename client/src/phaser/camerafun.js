9fc1a49d76c505cb2940c647a257c7af75d52586
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-28 21:51:06 -0400

    Background style manager now totally dynamic for any number of styles, with everything wrapped by 2D array (each style, each quadrant). Will be useful later with different simulations/game worlds.

--- client/src/phaser/camerafun.js
+++ client/src/phaser/camerafun.js
@@ -11,51 +11,63 @@ class Camerafun extends Phaser.Scene
 
     preload () 
     {
-        this.load.image('background', floral);
-        this.load.image('background2', floral2);
+        this.backgrounds = ['background', 'background2'];
+        this.load.image(this.backgrounds[0], floral);
+        this.load.image(this.backgrounds[1], floral2);
     }
 
     create () 
     {
         const cam = this.cameras.main.setBounds(0, 0, 1440, 1440);
 
-
-
+        let numStyles = 2;
         this.bgs = [];
-        this.bgs.push(this.add.image(0, 0, 'background').setOrigin(0));
-        this.bgs.push(this.add.image(720, 0, 'background').setOrigin(0));
-        this.bgs[1].flipX=true;
-        this.bgs.push(this.add.image(0, 720, 'background').setOrigin(0));
-        this.bgs[2].flipY=true;
-        this.bgs.push(this.add.image(720, 720, 'background').setOrigin(0));
-        this.bgs[3].flipX=true;
-        this.bgs[3].flipY=true;
-
-        for (const background of this.bgs) {
-            background.setDisplaySize(2048, 2048);
-            background.setScale(0.5);
-            background.alpha = 0.4;
+        for (let i=0; i < numStyles; i++)  {
+            let newBg = this.backgrounds[i];
+            //four quadrants of background
+            const bg = new Array(4);
+            bg[0] = this.add.image(0, 0, newBg).setOrigin(0);
+            bg[1] = this.add.image(720, 0, newBg).setOrigin(0);
+            bg[1].flipX=true;
+            bg[2] = this.add.image(0, 720, newBg).setOrigin(0);
+            bg[2].flipY=true;
+            bg[3] = this.add.image(720, 720, newBg).setOrigin(0);
+            bg[3].flipX=true;
+            bg[3].flipY=true;
+            for (const quadrant of bg) {
+                quadrant.setDisplaySize(2048, 2048);
+                quadrant.setScale(0.5);
+                quadrant.alpha = 0.4;
+            }
+            this.bgs.push(bg);
         }
 
-        this.bgs2 = [];
-        this.bgs2.push(this.add.image(0, 0, 'background2').setOrigin(0));
-        this.bgs2.push(this.add.image(720, 0, 'background2').setOrigin(0));
-        this.bgs2[1].flipX=true;
-        this.bgs2.push(this.add.image(0, 720, 'background2').setOrigin(0));
-        this.bgs2[2].flipY=true;
-        this.bgs2.push(this.add.image(720, 720, 'background2').setOrigin(0));
-        this.bgs2[3].flipX=true;
-        this.bgs2[3].flipY=true;
+        // for (const background of this.bgs) {
+        //     background.setDisplaySize(2048, 2048);
+        //     background.setScale(0.5);
+        //     background.alpha = 0.4;
+        // }
 
-        for (const background of this.bgs2) {
-            background.setDisplaySize(2048, 2048);
-            background.setScale(0.5);
-            background.alpha = 0.2;
-        }
+        // this.bgs2 = [];
+        // this.bgs2.push(this.add.image(0, 0, 'background2').setOrigin(0));
+        // this.bgs2.push(this.add.image(720, 0, 'background2').setOrigin(0));
+        // this.bgs2[1].flipX=true;
+        // this.bgs2.push(this.add.image(0, 720, 'background2').setOrigin(0));
+        // this.bgs2[2].flipY=true;
+        // this.bgs2.push(this.add.image(720, 720, 'background2').setOrigin(0));
+        // this.bgs2[3].flipX=true;
+        // this.bgs2[3].flipY=true;
+
+        // for (const background of this.bgs2) {
+        //     background.setDisplaySize(2048, 2048);
+        //     background.setScale(0.5);
+        //     background.alpha = 0.2;
+        // }
         
         this.maxOpacity = 0.5;
-        this.bgOpacity = this.maxOpacity;
-        this.bgOpacity2 = 0;
+        this.bgOpacity = new Array(numStyles);
+        this.bgOpacity[0] = this.maxOpacity;
+        this.bgOpacity[1] = 0;
         this.bgOpacityDirection = 1;
 
 
@@ -65,22 +77,22 @@ class Camerafun extends Phaser.Scene
     
         // this.cameras.main.originX = 1;
         this.cameras.main.centerToSize();
-        cam.setBackgroundColor(0xbababa);
+        cam.setBackgroundColor(0xffffff);
     }
 
     update () 
     {
-        if (this.bgOpacity >= this.maxOpacity)
+        if (this.bgOpacity[0] >= this.maxOpacity)
             this.bgOpacityDirection = -1;
-        else if (this.bgOpacity <= 0)
+        else if (this.bgOpacity[0] <= 0)
             this.bgOpacityDirection = 1;
   
-        this.bgOpacity += 0.001 * this.bgOpacityDirection;
-        this.bgOpacity2 += 0.001 * this.bgOpacityDirection * -1;
+        this.bgOpacity[0] += 0.001 * this.bgOpacityDirection;
+        this.bgOpacity[1] += 0.001 * this.bgOpacityDirection * -1;
         
-        this.bgs.forEach(bg => { bg.alpha = this.bgOpacity })
-        this.bgs2.forEach(bg => { bg.alpha = this.bgOpacity2 })
-        
+        for (let i in this.bgs) {
+            this.bgs[i].forEach(quadrant => { quadrant.alpha = this.bgOpacity[i] }) ;
+        }
 
         const cam = this.cameras.main;
     
