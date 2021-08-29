3b5a6ab899857da14928ddba7ccab5341b20be8f
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-29 03:29:00 -0400

    React-Spring installed. Popout component created that takes in children. Anything surrounded by Popout tags will, as the name might suggest, pops out.

---
+++ client/src/components/Popout.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from "react";
+import { useSpring, animated } from "react-spring";
+import "../index.css";
+
+function Popout({ children }) {
+    
+    const [animatedProps, setAnimatedProps] = useSpring(() => {
+        return {
+            coords: [1, 1, 1],
+            // react-spring physics
+            config: { mass: 5, tension: 150, friction: 30, precision: 0.00002 }
+        };
+    });
+    
+    // hover state
+    const [hoveredOver, setHoveredOver] = useState(false);
+
+    const ref = useRef(); 
+    return (
+      <animated.div
+        className="popout"
+        onMouseEnter={() => setHoveredOver(true)}
+        ref={ref}
+        style={{
+            //Hovered element pops out over other elements
+            zIndex: hoveredOver ? 2 : 1,
+            transform: animatedProps.coords.to((xCoord, yCoord, scale) =>
+            `perspective(1000px) rotateX(${xCoord}deg) rotateY(${yCoord}deg) scale(${scale})`)
+        }}
+        onMouseMove={({ clientX, clientY }) => {
+          //Mouse
+          const xCoord = clientX - (ref.current.offsetLeft - (window.scrollX || window.pageXOffset || document.body.scrollLeft));
+          const yCoord = clientY - (ref.current.offsetTop - (window.scrollY || window.pageYOffset || document.body.scrollTop));
+  
+          const coords = [-1 * (yCoord - ref.current.clientHeight / 2) / 80, 
+            (xCoord - ref.current.clientWidth / 2) / 80,
+            1.15 //Scaling factor for hovered work card
+          ];
+          setAnimatedProps({ coords: coords });
+        }}
+        onMouseLeave={() => {
+          setHoveredOver(false);
+          // Reset coordinates
+          setAnimatedProps({ coords: [0, 0, 1] });
+        }}
+      >
+        {children}
+      </animated.div>
+    );
+  }
+
+  export default Popout;
--- client/src/pages/LabPage.js
+++ client/src/pages/LabPage.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from "react-router-dom";
 import PhaserWorld from '../components/PhaserWorld';
 import { Figure, Image, Caption, Jumbotron } from 'react-bootstrap';
-import image from '../assets/star-dna-banner.jpg';
+import image from '../assets/banner3.jpg';
+import Popout from '../components/Popout';
+
 
 
 const LabPage = () => {
@@ -22,18 +24,20 @@ const LabPage = () => {
             <Jumbotron>
 				<h2>Lab</h2>
 			</Jumbotron>
-            <Figure>
-            <Figure.Image
-                width={171}
-                height={180}
-                alt="171x180"
-                src={image}
-                roundedCircle
-            />
-            <Figure.Caption>
-                Lab
-            </Figure.Caption>
-            </Figure>
+                <Figure>
+                    <Popout>
+                <Figure.Image
+                    width={171}
+                    height={180}
+                    alt="171x180"
+                    src={image}
+                    roundedCircle
+                />
+            </Popout>
+                <Figure.Caption>
+                    Lab
+                </Figure.Caption>
+                </Figure>
             < PhaserWorld width={600} height={600} worldType="Camerafun"/>
             < PhaserWorld width={200} height={150} worldType="Colorfun"/>
             < PhaserWorld width={400} height={275} worldType="Shaderfun"/>
