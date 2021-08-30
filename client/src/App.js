3c326405a2c6b7aa76a4fa4534000495de9e46ea
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-29 21:41:36 -0400

    Building HomePage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
 import LabPage from './pages/LabPage';
 import EcosystemPage from './pages/EcosystemPage';
 import GamePage from './pages/GamePage';
@@ -13,7 +14,7 @@ function App() {
       <>
         <Navigation />
         <Switch>
-          <Route exact path='/'> <h1>Welcome</h1> </Route>
+          <Route exact path='/' component={HomePage} />
           <Route exact path='/ecosystem' component={EcosystemPage} />
           <Route exact path='/lab' component={LabPage} />
           <Route exact path='/game' component={GamePage} />
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React, { Suspense } from 'react'
-import { useHistory } from "react-router-dom";
-import { Figure, Image, Caption } from 'react-bootstrap';
+import { useHistory, Link } from "react-router-dom";
+import { Card, Figure, Image, Caption } from 'react-bootstrap';
 import Popout from '../components/Popout';
 import { Button } from 'react-bootstrap';
 import SideNav from '../components/Controls';
+import ecoImage from '../assets/ecopage.png';
+import image from '../assets/ecopage.png';
 
 
 
 
-const LabPage = () => {
+
+const HomePage = () => {
 
     const history = useHistory()
     const previous = () => { history.goBack() }
@@ -20,26 +23,87 @@ const LabPage = () => {
     return (
         <div>
             <Button variant="danger" class="clickable" onClick={previous}>Back</Button>
-            <SideNav />
-                <Figure>
-                <Figure.Image
-                    width={171}
-                    height={180}
-                    alt="171x180"
-                    src={image}
-                    roundedCircle
-                />
-                <Figure.Caption>
-                    Lab
-                </Figure.Caption>
+            <h1>Welcome</h1>
+            <Card
+                bg="dark"
+                text="light"
+                style={{ width: '15rem' }}
+                className="mb-2 pageCard"
+            >
+                <Card.Header>Ecosystem</Card.Header>
+                <Card.Body>
+                <Popout>
+                        <Link as={Link} to='/ecosystem'>
+                            <Figure.Image
+                                width={171}
+                                height={380}
+                                alt="171x180"
+                                src={ecoImage}
+                                roundedCircle
+                                />
+                        </Link>
+                    </Popout>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+
+                <Figure class="pageCard">
+                    <Popout>
+                        <Link as={Link} to='/ecosystem'>
+                            <Figure.Image
+                                style={{ width: '18rem' }}
+                                alt="171x180"
+                                src={ecoImage}
+                                roundedCircle
+                                />
+                        </Link>
+                    </Popout>
+                    <Figure.Caption>
+                        Ecosystem
+                    </Figure.Caption>
                 </Figure>
-            <Suspense fallback={<div>Loading...</div>}>
+                <Figure class="pageCard">
+                    <Popout>
+                        <Link as={Link} to='/lab'>
+                            <Figure.Image
+                                width={171}
+                                height={380}
+                                alt="171x180"
+                                src={image}
+                                roundedCircle
+                                />
+                        </Link>
+                    </Popout>
+                    <Figure.Caption variant="light">
+                        Lab
+                    </Figure.Caption>
+                </Figure>
+                <Figure class="pageCard">
+                    <Popout>
+                        <Link as={Link} to='/game'>
+                            <Figure.Image
+                                width={171}
+                                height={380}
+                                alt="171x180"
+                                src={image}
+                                roundedCircle
+                                />
+                        </Link>
+                    </Popout>
+                    <Figure.Caption>
+                        Game
+                    </Figure.Caption>
+                </Figure>
+            {/* <Suspense fallback={<div>Loading...</div>}>
                 < PhaserWorld width={600} height={600} worldType="Camerafun"/>
                 < PhaserWorld width={200} height={150} worldType="Colorfun"/>
                 < PhaserWorld width={400} height={275} worldType="Shaderfun"/>
-            </Suspense>
+            </Suspense> */}
         </div>
     )
 };
 
-export default LabPage;
+export default HomePage;
