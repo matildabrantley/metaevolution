d3ac34c2c1665282fba0fd1cf712b2b354681bd3
Matilda Brantley <matildahbrantley@gmail.com>
2021-11-10 23:56:07 -0500

    Cleaned up unused code in server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 const express = require('express');
-//const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
-//const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
