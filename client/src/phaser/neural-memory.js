02d6370c9f9142fd9ccd94c125703efe9130f5fb
Matilda Brantley <matildahbrantley@gmail.com>
2021-12-04 23:53:47 -0500

    Added methods getVector (1D slice of any dimension) and getVectorSumAtIntersection (sum of all intersecting vectors in each dimension).

--- client/src/phaser/neural-memory.js
+++ client/src/phaser/neural-memory.js
@@ -1,18 +1,18 @@
 
 class NeuralMemory {
     constructor (numDimensions, width) {
+        this.numDimensions = numDimensions;
         this.width = width;
         this.memory = buildNDimensionalCube(numDimensions);
     }
-    
-    () {
-        
-    }
-    
+
     //builds and returns multidimensional array with n-cube shape (n dimensions with equal width i.e. square, cube, tesseract..)
-    function buildNDimensionalCube(numDimensions) {
+    buildNDimensionalCube(numDimensions) {
+        //Equal width dimensions preferred due to the way the network reads and writes vectors in dynamic memory
+        //and how the memory capacity grows through generations during evolution. 
+        //In other words, complete symmetry is favorable.
         if (numDimensions > 1) {
-            var arr = new Array();
+            let arr = new Array();
             for (let i = 0; i < this.width; i++) {
                 //recursively build lower dimensions
                 arr[i] = this.buildNDimensionalCube(numDimensions - 1);
@@ -21,6 +21,19 @@ class NeuralMemory {
          } else {
             return new Array(this.width).fill(0);
          }
+    }
+
+    //get a vector that's the pointwise sum of all vectors intersecting that point in the n-cube
+    getVectorSumAtIntersection(point) {
+        
+    }
+
+    //get a vector that's a 1D slice from any dimension of the n-cube (row, column, etc.)
+    //dimension = 0 means rows, 1 means columns and so forth
+    getVector(dimension, index) {
+        switch (dimension) {
+            case 0:
+                return this.memory[index];
     }
     
 }
