5d43960ff851e1d961b5caefbce8f4de32bdc055
Matilda Brantley <matildahbrantley@gmail.com>
2021-12-06 13:30:59 -0500

    Descriptions of each vector operation in NeuralMemory.

--- client/src/phaser/neural-memory.js
+++ client/src/phaser/neural-memory.js
@@ -23,57 +23,61 @@ class NeuralMemory {
          }
     }
 
-    //get a vector that's the pointwise sum of all vectors intersecting that point in the n-cube
-    getVectorSumAtIntersection(point) { //point is an array of length numDimensions containing indices
-        let sum = new Array(this.width).fill(0);
-        //Add up all vectors intersecting the point
-        for (let i = 0; i < this.numDimensions; i++) 
-            sum = this.addVectors(sum, getVector(point, i));
-        return sum;
-    }
+    // *** Basic Vector Operations *** 
 
+    // Two Vectors Operations
+    // return sum of two vectors
     addTwoVectors(vector1, vector2) {
         for (let i = 0; i < vector1.length; i++) 
             vector1[i] = vector1[i] + vector2[i];
         return vector1;
     }
-
+    //return difference of two vectors
     subtractTwoVectors(vector1, vector2) {
         for (let i = 0; i < vector1.length; i++)
             vector1[i] = vector1[i] - vector2[i];
         return vector1;
     }
-
+    //return product of two vectors
     multiplyTwoVectors(vector1, vector2) {
         for (let i = 0; i < vector1.length; i++)
             vector1[i] = vector1[i] * vector2[i];
         return vector1;
     }
-
+    //return quotient of two vectors
     divideTwoVectors(vector1, vector2) {
         for (let i = 0; i < vector1.length; i++)
             vector1[i] = vector1[i] / vector2[i];
         return vector1;
     }
 
+    //Scalar Vector Operations
+    //return sum of a vector with scalar
     addVector(vector, scalar) {
         for (let i = 0; i < vector.length; i++)
             vector[i] = vector[i] + scalar;
         return vector;
     }
-
+    //return difference of a vector and scalar
+    subtractVector(vector, scalar) {
+        for (let i = 0; i < vector.length; i++)
+            vector[i] = vector[i] - scalar;
+        return vector;
+    }
+    //return product of a vector and scalar
     multiplyVector(vector, scalar) {
         for (let i = 0; i < vector.length; i++)
             vector[i] = vector[i] * scalar;
         return vector;
     }
-
+    //return quotient of a vector and scalar
     divideVector(vector, scalar) {
         for (let i = 0; i < vector.length; i++)
             vector[i] = vector[i] / scalar;
         return vector;
     }
 
+    //Average any number of vectors
     averageVectors(vectors) {
         let sum = new Array(this.width).fill(0);
         for (let i = 0; i < vectors.length; i++)
@@ -81,19 +85,29 @@ class NeuralMemory {
         return this.divideVector(sum, vectors.length);
     }
 
-    //get a vector that's a 1D slice from any dimension of the n-cube (row, column, etc.)
-    //dimension = 0 means rows, 1 means columns and so forth
-    getVector(point, dimension) { //point is an array of length numDimensions containing indices
+    //get a vector that's the pointwise sum of all vectors intersecting that point in the n-cube
+    getVectorSumAtIntersection(point) { //point is an array of length numDimensions containing indices
+        let sum = new Array(this.width).fill(0);
+        //Add up all vectors intersecting the point
+        for (let i = 0; i < this.numDimensions; i++) 
+        sum = this.addTwoVectors(sum, getVectorByPoint(point, i));
+        return sum;
+    }
+
+    //get a vector that's a 1D slice from any direction of the n-cube (row, column, etc.)
+    //by passing a point inside that vector and the direction of the slice
+    getVectorByPoint(point, direction) { //point is an array of length numDimensions containing indices
         let v = Array(this.width);
 
         //Up to 8 dimensions supported
         //these nested switches are a mess right now, but it's quite fast
-        //need a solution with less repetitive code and scalable dimensions, so perhaps a different data structure
+        //need a solution with less repetitive code and scalable dimensions, 
+        //so something recursive or perhaps a different data structure
         //the difficulty is needing to get a vector from any slice for all possible dimensionalities
         switch(this.numDimensions) {
             case 2:
                 //2D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++) 
                             v[i] = this.memory[i][point[1]];
@@ -106,7 +120,7 @@ class NeuralMemory {
                 break;
             case 3:
                 //3D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]];
@@ -122,7 +136,7 @@ class NeuralMemory {
                 }
             case 4:
                 //4D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]][point[3]];
@@ -142,7 +156,7 @@ class NeuralMemory {
                 break;
             case 5:
                 //5D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]][point[3]][point[4]];
@@ -167,7 +181,7 @@ class NeuralMemory {
                 break;
             case 6:
                 //6D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]][point[3]][point[4]][point[5]];
@@ -196,7 +210,7 @@ class NeuralMemory {
                 break;
             case 7:
                 //7D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]][point[3]][point[4]][point[5]][point[6]];
@@ -229,7 +243,7 @@ class NeuralMemory {
                 break;
             case 8:
                 //8D case
-                switch (dimension) {
+                switch (direction) {
                     case 0:
                         for (let i=0; i < this.width; i++)
                             v[i] = this.memory[i][point[1]][point[2]][point[3]][point[4]][point[5]][point[6]][point[7]];
