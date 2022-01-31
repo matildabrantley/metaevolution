ad5e973653ccea49280c1f2f241ce57dd4c36033
Matilda Brantley <matildahbrantley@gmail.com>
2022-01-30 23:57:21 -0500

    RelativeMemory class allows for dynamic construction of relationships within memory (which is an n-dimensional memory object passed along with array of relationships).

--- client/src/neural/relative-memory.js
+++ client/src/neural/relative-memory.js
@@ -1,15 +1,30 @@
 import NDimensionMemory from './n-dimension-memory'
 
-class RelativeMemory extends NDimensionMemory {
-    
-    constructor(numDimensions = 1, width = 5) {
-        super(numDimensions, width);
-        this.relationships = [];
+class RelativeMemory {
+    //pass in n-dimensional memory
+    constructor(memory, ...relationships) {
+        this.memory = memory;
+        this.relationships = relationships;
     }
     
     findVectorFromRelationship() {
-        
+        let vector = [];
+        for (let i = 0; i < this.relationships.length; i++) {
+            vector.push(this.relationships[i].findVector());
+        }
+        return vector;
     }
+
+    findVectorFromMemory() {
+        for (let i = 0; i < this.memory.length; i++) {
+            let vector = this.memory[i];
+            if (vector.length === this.relationships.length) {
+                return vector;
+            }
+        }
+    }
+
+
 
 }
 
